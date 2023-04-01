326848766154e6409c996548f5474b5073ead0e3
Jason Huang <jason97521@gmail.com>
2023-04-01 02:54:44 -0400

    hover detect upload, tag to post ui

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,34 +1,26 @@
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
 import * as faceapi from 'face-api.js'
 
 const NewPost = ({image}) => {
     const {url,width,height} = image
+    const [faces,setFaces] = useState([])
+    const [friends,setFriends] = useState([])
 
     const imgRef = useRef()
     const canvasRef = useRef()
 
     const handleImage = async () => {
-    const detections = await faceapi.detectAllFaces(
-        imgRef.current, 
-        new faceapi.TinyFaceDetectorOptions()
-    ).withFaceLandmarks()
-    .withFaceExpressions()
-
-    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current)
-    faceapi.matchDimensions(canvasRef.current, {
-        width,
-        height,
-    })
-
-    const resized = faceapi.resizeResults(detections,{
-        width,
-        height,
-    })
-
-    faceapi.draw.drawDetections(canvasRef.current, resized)
-    faceapi.draw.drawFaceExpressions(canvasRef.current, resized)
-    faceapi.draw.drawFaceLandmarks(canvasRef.current, resized)
+        const detections = await faceapi.detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
+        setFaces(detections.map(d=>Object.values(d.box)))
     }
+
+    const enter = () =>{
+        const ctx = canvasRef.current.getContext("2d")
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "yellow"
+        faces.map(face=> ctx.strokeRect(...face))
+    }
+
   useEffect(()=>{
     const loadModels = () =>{
       Promise.all([
@@ -42,11 +34,27 @@ const NewPost = ({image}) => {
 
     imgRef.current && loadModels()
   },[])
+
+  const addFriend = (e) =>{
+    setFriends(prev=>({...prev, [e.target.name]: e.target.value}))
+  }
+
+  console.log(friends)
   return (
     <div className="container">
         <div className="left" style={{width,height}}>
             <img ref={imgRef} crossOrigin="anonymous" src={url} alt="" />
-            <canvas ref={canvasRef} width={width} height={height} />
+            <canvas onMouseEnter={enter} ref={canvasRef} width={width} height={height} />
+            {faces.map((face,i)=>(
+                <input 
+                    name={`input${i}`}
+                    style={{left:face[0], top:face[1] + face[3] + 5}} 
+                    placeholder="Tag a friend" 
+                    key={i} 
+                    className="friendInput"
+                    onChange={addFriend}
+                />
+            ))}
         </div>
         <div className="right">
             <h1>Share your post</h1>
@@ -55,6 +63,11 @@ const NewPost = ({image}) => {
                 placeholder="What's on your mind?"
                 className="rightInput"
             />
+            {friends && (
+                <span className="friends">
+                    with <span className="name">{Object.values(friends) + " "}</span>
+                </span>
+            )}
             <button className="rightButton">Send</button>
         </div>
     </div>
