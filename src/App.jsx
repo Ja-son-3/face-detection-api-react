f8b1bbfee3aa6f2e8c1ef241d5a83cb7362e9378
Jason Huang <jason97521@gmail.com>
2023-04-01 01:29:05 -0400

    detect face, expressions and face landmarks

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,32 @@ const App = () => {
   const canvasRef = useRef()
 
   const handleImage = async () => {
-    
+    const detections = await faceapi.detectAllFaces(
+      imgRef.current, 
+      new faceapi.TinyFaceDetectorOptions()
+    ).withFaceLandmarks()
+    .withFaceExpressions()
+
+    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current)
+    faceapi.matchDimensions(canvasRef.current, {
+      width: 940,
+      height: 650,
+    })
+
+    const resized = faceapi.resizeResults(detections,{
+      width: 940,
+      height: 650,
+    })
+
+    faceapi.draw.drawDetections(canvasRef.current, resized)
+    faceapi.draw.drawFaceExpressions(canvasRef.current, resized)
+    faceapi.draw.drawFaceLandmarks(canvasRef.current, resized)
   }
   useEffect(()=>{
     const loadModels = () =>{
       Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
         faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
         faceapi.nets.faceExpressionNet.loadFromUri("/models"),
       ])
       .then(handleImage)
@@ -25,7 +43,14 @@ const App = () => {
   },[])
   return (
     <div className="app">
-      <img ref={imgRef} src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" width="940" height="650" />
+      <img 
+        crossOrigin="anonymous"
+        ref={imgRef} 
+        src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        alt="" 
+        width="940" 
+        height="650" 
+      />
       <canvas ref={canvasRef} width="940" height="650"></canvas>
     </div>
   )
